843fb5bba2ac2140728d10f60359ae6f44ec58f5
AnishChacko <anishchacko2204@gmail.com>
2023-12-29 16:58:38 -0600

    updatede

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 import java.util.*;
+import java.util.Scanner;
+import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("register");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Please select your preferred choice:");
+        System.out.println("1. Register Account");
+        System.out.println("2. Book Reservation");
+        System.out.println("3. Generate Bill");
+        System.out.println("4. Feedback");
     }
 }
